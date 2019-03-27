0131a40a8bede0a732b33156b6f55e707d070700
Link-D <linkaiyuan.dai@free.fr>
2019-03-27 21:59:19 +0100

    Update Gameboard_temp.py
    Changement de quelques trucs de GUI:
    • Couleurs (cases et clics)
    • Rotation tout seul

--- Gameboard_temp.py
+++ Gameboard_temp.py
@@ -9,7 +9,6 @@
 import tkinter as tk
 from math import cos, sin, asin, pi
 from random import choice
-from time import sleep
 
 
 # #########
@@ -17,12 +16,17 @@ from time import sleep
 # #########
 
 # Dimensions du plateau de jeu
-vert_scale = 7  # facteur d'étirement vertical (effet 3D) [modifiable]
-win_dim = min((660 * 19 / 20) / vert_scale, 1260)  # dimensions maximales
+_game_win = tk.Tk()  # fenêtre principale
+scrn_w = _game_win.winfo_screenwidth() - 20  # largeur de l'écran
+scrn_h = _game_win.winfo_screenheight() - 20  # longueur de l'écran
+_game_win.title("Honey Emblem")  # nom du jeu
+vert_scale = 0.5  # facteur d'étirement vertical (effet 3D) [modifiable]
+win_dim = min((scrn_h * 19 / 20) / vert_scale, scrn_w)  # dimensions maximales
 space = win_dim / 20  # espace vide au-dessus du plateau (effet 3D)
 center = win_dim / 2  # case du milieu (référentiel)
 board_side = 6  # nombre de cases sur un côté du plateau hexagonal [modifiable]
 rotate = 0  # rotation du plateau en radians [modifiable]
+rotate_delay = 15  # temps en ms entre 2 mises à jour du plateau [modifiable]
 
 # Dimensions des cases
 tl_side = win_dim / (2 * (3 * board_side ** 2 - 3 * board_side + 2) ** 0.5)
@@ -32,6 +36,9 @@ tl_size = tl_side * (3 ** 0.5)
 gameboard = [[0]]
 for layer in range(1, board_side):
     gameboard.append(layer * 6 * [0])
+
+# Liste des personnages
+characters = list(gameboard)
 
 
 # ##########
@@ -49,21 +56,26 @@ def Hex_points(tile):
 
 # Couleurs d'un case [provisoire]
 def Tile_type():
-    return choice(["#ff0000", "#00ff00", "#0000ff",
-                   "#00ffff", "#ff00ff", "#ffff00"])
+    return choice(["#ff0000", "#00ff00", "#0000ff"])
 
 
 # Rotation du plateau complet
-def Rotate_board(mouse):
+def Rotate_board():
     global rotate
     rotate = (rotate + pi / 1440) % (2 * pi)
 
-    # mise à jour de la position de chaque case
+    # mise à jour de la position de chaque case et de chaque personnage
     for layer in gameboard:
         for tile in layer:
             Tile.Position(tile)
-            
-    sleep(0.005)
+    # afficher les personnages devant les cases [à compléter pour perspective]
+    for layer in gameboard:
+        for tile in layer:
+            if tile.has_char:
+                _gameboard.tag_raise(tile.char_gui)
+
+    # attend avant de tourner de nouveau
+    _gameboard.after(rotate_delay, Rotate_board)
 
 
 # ######
@@ -103,6 +115,11 @@ class Tile:
         _gameboard.tag_bind(self.gui, "<Enter>", self.Cursor_enter)
         _gameboard.tag_bind(self.gui, "<Leave>", self.Cursor_leave)
 
+        # création d'un personnage sur la case [provisoire]
+        self.has_char = False
+        if not self.color == "#0000ff":
+            Tile.Create_char(self)
+
         # calcul de la position de la case sur le canevas
         Tile.Position(self)
 
@@ -136,25 +153,53 @@ class Tile:
         # création de la case sur le plateau
         _gameboard.coords(self.gui, self.disp)
 
+        # mise à jour de l'emplacement du personnage
+        if self.has_char:
+            self.Char_position()
+
     # Lorsqu'une case est cliquée
     def Cursor_click(self, mouse):
         # basculer entre couleur d'origine et blanc [provisoire]
         if self.clicked:
             self.type = self.color
         else:
-            self.type = "White"
+            self.type = "#ffffff"
         _gameboard.itemconfig(self.gui, fill=self.type, outline=self.type)
-        self.clicked = not(self.clicked)
+        self.clicked = not self.clicked
 
     # Lorsque la souris est au-dessus d'une case
     def Cursor_enter(self, mouse):
-        # colorer en noir [provisoire]
-        _gameboard.itemconfig(self.gui, fill="Black", outline="Black")
+        self.type1 = self.color.replace("00", "aa")
+        _gameboard.itemconfig(self.gui, fill=self.type1, outline=self.type1)
 
     # Lorsque la souris n'est plus au-dessus d'une case
     def Cursor_leave(self, mouse):
-        # rendre couleur d'origine [provisoire]
-        _gameboard.itemconfig(self.gui, fill=self.type,  outline=self.type)
+        _gameboard.itemconfig(self.gui, fill=self.type, outline=self.type)
+
+    # Création d'un personnage sur la case
+    def Create_char(self):
+
+        # la case contient un personnage !
+        self.has_char = True
+
+        # couleur du personnage [provisoire]
+        self.char_type = choice(["white", "black"])
+
+        # positionnement du personnage sur le plateau [provisoire]
+        self.char_gui = _gameboard.create_rectangle(0, 0, 0, 0,
+                                                    fill=self.char_type,
+                                                    outline=self.char_type)
+
+        # actions sur le personnage
+        _gameboard.tag_bind(self.char_gui, "<Button-1>", self.Cursor_click)
+        _gameboard.tag_bind(self.char_gui, "<Enter>", self.Cursor_enter)
+        _gameboard.tag_bind(self.char_gui, "<Leave>", self.Cursor_leave)
+
+    # Positionnement du personnage sur le plateau
+    def Char_position(self):
+        _gameboard.coords(self.char_gui, self.x - 0.25 * tl_size,
+                          self.disp_y - space, self.x + 0.25 * tl_size,
+                          self.disp_y)
 
 
 # Les personnages
@@ -166,36 +211,33 @@ class Character:
         self.type = choice(["white", "black"])
 
         # création de cubemen
-        _gameboard.create_rectangle(tile.x - (0.25 * tl_size),
-                                    tile.disp_y - (0.25 * tl_side),
-                                    tile.x + (0.25 * tl_size),
-                                    tile.disp_y + (0.25 * tl_side),
-                                              fill=self.type)
-                                              
+        self.gui = _gameboard.create_rectangle(0, 0, 0, 0,
+                                               fill=self.type,
+                                               outline=self.type)
+        self.Position(self, tile)
+
+    def Position(self, tile):
+        _gameboard.coords(self.gui, tile.x - 0.25 * tl_size,
+                          tile.disp_y - space, tile.x + 0.25 * tl_size,
+                          tile.disp_y)
+
 
 # ###################
 # Programme principal
 # ###################
-
-# Fenêtre de jeu
-_game_win = tk.Tk()
-_game_win.title("Honey Emblem")
 
 # Canevas sur lequel se déroulera le jeu
 _gameboard = tk.Canvas(_game_win, width=win_dim,
                        height=win_dim * vert_scale + space, bg="#000000")
 _gameboard.pack()
 
-#Appeler le personnage
-
 # Tourner le plateau quand la souris bouge [provisoire]
-# _gameboard.bind("<Motion>", Rotate_board)
+_gameboard.after(10, Rotate_board)
 
 # Création des cases
 for layer in gameboard:
     for tile in range(len(layer)):
         layer[tile] = Tile(gameboard.index(layer), tile)
-        Character(layer[tile])
 
 # Création de la fenêtre
 _game_win.mainloop()
